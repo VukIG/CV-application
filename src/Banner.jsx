8a125ead02c2808f17b2cfc19f7a0bd0c3f230c1
VukIG <vuk.ignjatovic05@gmail.com>
2023-01-10 20:12:11 -0900

    tailwind + vite

---
+++ src/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='flex justify-center text-[30px] color-white my-[20px]'>Banner</div>
+  )
+}
+
+export default Banner
